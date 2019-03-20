fceea023d2a1fbfa771c8ab0525733f213772087
김형진/SK 테크엑스(twinkle@sk.com) <twinkle@sk.com>
2019-03-20 19:37:18 +0900

    controller to context

--- snowgie.go
+++ snowgie.go
@@ -5,13 +5,13 @@ type Snowgie interface {
 	Run() error
 }
 
-type SnowgieController interface {
+type SnowgieContext interface {
 	ProcessPublish(id string , body []byte , priority uint8) error
 	GetConfig() SnowgieConfig
 }
 
 type SnowgieRuntime interface {
-	Init(controller SnowgieController) error
+	Init(ctx SnowgieContext) error
 	ProcessConsume(id string, body []byte)(error , bool)
 
 }
